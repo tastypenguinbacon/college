668d99309e2d6a0ecb19dac5bc8e6551af6d70da
pingwin <jaloszewski.adrian@gmail.com>
2016-10-16 23:33:26 +0200

    script is ready

--- SP/szoste/script/sixth.py
+++ SP/szoste/script/sixth.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
-x = []
+length = []
 U_AC = []
 U_DC = []
 
@@ -34,7 +34,7 @@ def get_values():
         for row in reader:
             if not is_number(row[0]):
                 continue
-            x.append(float(row[0]))
+            length.append(float(row[0]))
             U_AC.append(float(row[1]))
             U_DC.append(float(row[2]))
 
@@ -46,26 +46,31 @@ def configure_plot(i=iter(range(1,20))):
     plt.grid(True)
 
 
-def abs_error():
+def abs_error(x, y, swap_point=-1000000.0):
     xx = np.array(x[:])
-    yy = np.array(U_AC[:])
+    yy = np.array(y[:])
     for i in range(len(xx)):
-        if xx[i] <= 62.2:
+        if xx[i] <= swap_point:
             yy[i] *= -1
     a, b = linear_regression(xx, yy)
     calculated = a * xx + b
-
-    configure_plot()
-    plt.plot(xx, yy - calculated)
-    plt.show()
+    nonlinear = max(abs(yy - calculated)) / (max(yy) - min(yy))
+    print("Max error %f" % nonlinear)
+    return yy - calculated
 
 
 def uac_x():
     configure_plot()
-    plt.plot(x, U_AC, [62.2], [0.02648], 'ro')
+    plt.plot(length, U_AC, [62.2], [0.02648], 'ro')
     plt.ylabel("{\\Large $\\mathrm{U_{AC}}$ [V]}")
     plt.xlabel("{\\Large x [mm]}")
     plt.savefig("../img/Uac_od_x.png")
+    configure_plot()
+    error = abs_error(length, U_AC, 62.2) * 1000
+    plt.plot(length, error)
+    plt.ylabel("{\\Large $\\mathrm{\\Delta_U$ [mV]}")
+    plt.xlabel("{\\Large x [mm]}")
+    plt.savefig("../img/Uac_blad.png")
 
 
 def uac_udc():
@@ -74,14 +79,26 @@ def uac_udc():
     plt.ylabel("{\\Large $\\mathrm{U_{AC}}$ [V]}")
     plt.xlabel("{\\Large $\\mathrm{U_{DC}}$ [V]}")
     plt.savefig("../img/Uac_od_Udc.png")
+    configure_plot()
+    error = abs_error(U_DC, U_AC, 0.001) * 1000
+    plt.plot(U_DC, error)
+    plt.ylabel("{\\Large $\\mathrm{\\Delta_U$ [mV]}")
+    plt.xlabel("{\\Large $\\mathrm{U_{DC}}$ [V]}")
+    plt.savefig("../img/Uac_Udc_blad.png")
 
 
 def udc_x():
     configure_plot()
-    plt.plot(x, U_DC, [62.2], [0.001], 'ro')
+    plt.plot(length, U_DC, [62.2], [0.001], 'ro')
     plt.ylabel("{\\Large $\\mathrm{U_{DC}}$ [V]}")
     plt.xlabel("{\\Large x [mm]}")
     plt.savefig("../img/Udc_od_x.png")
+    configure_plot()
+    error = abs_error(length, U_DC) * 1000
+    plt.plot(length, error)
+    plt.ylabel("{\\Large $\\mathrm{\\Delta_U$ [mV]}")
+    plt.xlabel("{\\Large x [mm]}")
+    plt.savefig("../img/Udc_blad.png")
 
 
 def main():
@@ -89,7 +106,6 @@ def main():
         raise ValueError('Not enough parameters')
     get_values()
     uac_x()
-    abs_error()
     uac_udc()
     udc_x()
 
